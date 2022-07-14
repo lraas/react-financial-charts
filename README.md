301979bd4b9f5a5904b903ac12660bca9bc449fd
eugenejahn <eugenejahnjahn@gmail.com>
2022-07-13 20:08:41 -0700

    latest code

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 # React-Grid-Layout
 
 [![travis build](https://travis-ci.org/STRML/react-grid-layout.svg?branch=master)](https://travis-ci.org/STRML/react-grid-layout)
@@ -591,3 +592,72 @@ us easily isolate it.
 - [x] Min/max w/h per item
 - [x] Resizable handles on other corners
 - [ ] Configurable w/h per breakpoint
+=======
+# React Financial Charts
+
+Create highly customizable stock charts.
+
+Built with [React JS](http://facebook.github.io/react/) and [d3](http://d3js.org/)
+
+- integrates multiple chart types
+- over 60 technical indicators and overlays
+- drawing objects
+
+Multiple [quick start examples](https://github.com/rrag/react-stockcharts-examples2)
+
+- svg and canvas for improved performance.
+- pan and zoom, on touch devices too
+
+## Chart types
+
+- Scatter
+- Area
+- Line
+- Candlestick
+- OHLC
+- HeikenAshi
+- Renko
+- Kagi
+- Point & Figure
+
+## Indicators
+
+- EMA, SMA, WMA, TMA
+- Bollinger band
+- SAR
+- MACD
+- RSI
+- ATR
+- Stochastic (fast, slow, full)
+- ForceIndex
+- ElderRay
+- Elder Impulse
+
+## Interactive Indicators
+
+- Trendline
+- Fibonacci Retracements
+- Gann Fan
+- Channel
+- Linear regression channel
+
+---
+
+## Installation
+
+```sh
+npm install react-financial-charts
+```
+
+## Documentation
+
+TODO
+
+## Contributing
+
+Refer to [CONTRIBUTING.md](./CONTRIBUTING.md)
+
+## LICENSE
+
+[ISC](./LICENSE)
+>>>>>>> feat: initial conversion to typescript
--- packages/react-financial-charts/src/utils/shallowEqual.ts
+++ packages/react-financial-charts/src/utils/shallowEqual.ts
@@ -1,14 +1,9 @@
+// https://github.com/jonschlinkert/is-equal-shallow/
+
+/*
 The MIT License (MIT)
-<<<<<<< HEAD
 
-Copyright (c) 2016 Samuel Reed
-=======
-https://github.com/reactivemarkets/react-financial-charts
-
-Copyright (c) 2015-2018 Ragu Ramaswamy
-Copyright (c) 2016 Julien Renaux
-Copyright (c) 2019 Reactive Markets
->>>>>>> feat: initial conversion to typescript
+Copyright (c) 2015, Jon Schlinkert.
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -17,23 +12,47 @@ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 copies of the Software, and to permit persons to whom the Software is
 furnished to do so, subject to the following conditions:
 
-<<<<<<< HEAD
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-=======
 The above copyright notice and this permission notice shall be included in
 all copies or substantial portions of the Software.
->>>>>>> feat: initial conversion to typescript
 
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
 AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-<<<<<<< HEAD
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-=======
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
->>>>>>> feat: initial conversion to typescript
+*/
+
+function isDate(date) {
+    return Object.prototype.toString.call(date) === "[object Date]";
+}
+
+function isEqual(val1, val2) {
+    return (isDate(val1) && isDate(val2))
+        ? val1.getTime() === val2.getTime()
+        : val1 === val2;
+}
+
+export default function shallowEqual(a, b) {
+    if (!a && !b) { return true; }
+    if (!a && b || a && !b) { return false; }
+
+    let numKeysA = 0;
+    let numKeysB = 0;
+    let key;
+
+    // tslint:disable: forin
+    for (key in b) {
+        numKeysB++;
+        if ((b.hasOwnProperty(key) && !a.hasOwnProperty(key)) || !isEqual(a[key], b[key])) {
+            return false;
+        }
+    }
+
+    for (key in a) {
+        numKeysA++;
+    }
+
+    return numKeysA === numKeysB;
+}
